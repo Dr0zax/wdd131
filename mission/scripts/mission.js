9bb8e4dd61957d8254280ce48d306d444d7b0602
Dr0zax <geodashfan4@gmail.com>
2024-05-07 12:51:09 -0600

    added select element

--- mission/scripts/mission.js
+++ mission/scripts/mission.js
@@ -1,17 +1,10 @@
-const themeSelector = document.getElementById("theme-switcher")
+const themeSelector = document.getElementById("theme-selector")
 
 function changeTheme() {
-    if (themeSelector.getAttribute('value') == 'dark')
-    {
-        themeSelector.setAttribute('value', 'light')
-        document.body.className = 'light';
-        themeSelector.innerText = 'Light';
-        document.querySelector('.logo').setAttribute('src', 'images/byui-logo_blue.webp')
-    } else if (themeSelector.getAttribute('value') == 'light') {
-        themeSelector.setAttribute('value', 'dark')
-        document.body.className = 'dark';
-        themeSelector.innerText = 'Dark';
-        document.querySelector('.logo').setAttribute('src', 'images/byui-logo_white.png')
+    if (themeSelector.value === "dark") {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
     }
 }
 
