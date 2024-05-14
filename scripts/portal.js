74d504616d3ff291e0d88aa035ec39dd3655beda
Dr0zax <geodashfan4@gmail.com>
2024-05-14 13:27:25 -0600

    updated nav bar and added dark/light mode

--- scripts/portal.js
+++ scripts/portal.js
@@ -1,3 +1,21 @@
 const copyright_year = document.querySelector("#year");
 
 copyright_year.textContent = new Date().getFullYear();
+
+const themeSelector = document.querySelector("#theme-selector");
+if (themeSelector.value === "dark") {
+    document.body.classList.add("dark");
+} else {
+    document.body.classList.remove("dark");
+}
+
+
+function changeTheme() {
+    if (themeSelector.value === "dark") {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.remove("dark");
+    }
+}
+
+themeSelector.addEventListener("change", changeTheme)
