31125e32581d7c7681b6737cb7f141f7220e9a33
Dr0zax <geodashfan4@gmail.com>
2024-05-25 19:25:15 -0600

    Book blog

---
+++ book-blog/scripts/blog.js
@@ -0,0 +1,27 @@
+const articles = [
+	{
+		id: 1,
+		title: "Ender's Game",
+		date: 'January 15, 1985',
+		description:
+			'Ender\'s Game is a 1985 military science fiction novel byAmerican author Orson Scott Card. Set at an unspecified date in Earth\'s future, the novel presents an imperiled humankind after two conflicts with an insectoid alien species they dub \"the buggers\".',
+		imgSrc: 'https://m.media-amazon.com/images/I/91DCtJSKcKL._SL1500_.jpg',
+		imgAlt: 'Book cover for Enders Game',
+		ages: '13-14',
+		genre: 'Sci-Fi',
+		stars: '8/10'
+	},
+	{
+		id: 2,
+		title: 'Fabelhaven',
+		date: 'June 14, 2006',
+		description:
+			'For centuries, mystical creatures of all description were gathered to a hidden refuge called Fablehaven to prevent their extinction. The sanctuary is one of the last strongholds of true magic. Enchanting? Absolutely. Exciting? You bet. Safe? Well, actually, quite the opposite . . . Kendra and her brother, Seth, have no idea their grandfather is the current caretaker of Fablehaven. Inside the gated woods, ancient laws keep order among greedy trolls, mischievous satyrs, plotting witches, spiteful imps, and jealous fairies. However, when the rules get broken, powerful forces of evil are unleashed, forcing Kendra and Seth to face the greatest challenge of their lives, to save their family, Fablehaven, and perhaps even the world.',
+		imgSrc:
+			'https://m.media-amazon.com/images/I/81-CHrH4mCL._SY425_.jpg',
+		imgAlt: 'Book cover for Fabelhaven',
+		ages: '8+',
+		genre: 'Fantasy',
+		stars: '9/10'
+	}
+]
