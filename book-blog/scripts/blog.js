2e0aee4154cb9d437551e0eb819a4c805b9e828e
Dr0zax <geodashfan4@gmail.com>
2024-06-01 18:14:56 -0600

    W06 Prove

--- book-blog/scripts/blog.js
+++ book-blog/scripts/blog.js
@@ -4,7 +4,7 @@ const articles = [
 		title: "Ender\'s Game",
 		date: 'January 15, 1985',
 		description:
-			'Ender\'s Game is a 1985 military science fiction novel byAmerican author Orson Scott Card. Set at an unspecified date in Earth\'s future, the novel presents an imperiled humankind after two conflicts with an insectoid alien species they dub \"the buggers\".',
+			'Ender\'s Game is a 1985 military science fiction novel by American author Orson Scott Card. Set at an unspecified date in Earth\'s future, the novel presents an imperiled humankind after two conflicts with an insectoid alien species they dub \"the buggers\".',
 		imgSrc: 'https://m.media-amazon.com/images/I/91DCtJSKcKL._SL1500_.jpg',
 		imgAlt: 'Book cover for Enders Game',
 		ages: '13-14',
@@ -23,5 +23,51 @@ const articles = [
 		ages: '8+',
 		genre: 'Fantasy',
 		stars: '9/10'
+	},
+	{
+		id: 3,
+		title: 'Alcatraz vs the Evil Librarians',
+		date: 'October 1, 2007',
+		description: 'On his 13th birthday, Alcatraz, a foster child, gets a bag of sand in the mail which purports to be his “inheritance” sent from his father and mother. The Librarians, of course, immediately steal the bag of sand from him. This sparks a chain of events which leads Alcatraz to realize that his family is part of a group of freedom fighters who resist the Evil Librarians—the secret cult who actually rule the world. Alcatraz’s grandfather shows up and tows him off to infiltrate the downtown library to steal back the mystical bag of sand. The ensuing story involves talking dinosaurs, sentient romance novels, and a dungeon-like labyrinth hiding beneath the innocent-looking downtown library. With a seductive mix of offbeat humor, high adventure, and endearing characterization, this is a fantasy that will appeal to all lovers of the fantastical.',
+		imgSrc: "https://m.media-amazon.com/images/I/81Zfucd2l1L._SL1500_.jpg",
+		imgAlt: 'Book cover for Alcatraz vs the Evil Librarians',
+		ages: '8-13',
+		genre: 'Fantasy',
+		stars: '8/10'
 	}
-]
+];
+
+const catalog = document.querySelector('#catalog')
+
+const article_template = (bookTitle, bookDescription, publish_date, ageRange, genre, rating, bookImage, imgAlt) => {
+	return `<section class="book">
+	<section class="book-details">
+	  <h2>${bookTitle}</h2>
+	  <p>${publish_date}</p>
+	  <p>${ageRange}</p>
+	  <p>${genre}</p>
+	  <p>${rating}</p>
+	</section>
+	<section class="book-image-container">
+	  <section class="book-image">
+		<img src="${bookImage}" alt="${imgAlt}" />
+	  </section>
+	  <p class="book-description">
+		${bookDescription}
+	  </p>
+	</section>
+  </section>`
+}
+
+articles.forEach(article => {
+	catalog.innerHTML += article_template(
+		article.title,
+		article.description,
+		article.date,
+		article.ages,
+		article.genre,
+		article.stars,
+		article.imgSrc,
+		article.imgAlt
+	)
+})
